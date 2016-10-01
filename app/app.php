e68adba3f3697eebc55c7091f2d74be04519eee6
Ryan Loos <RLoos289@gmail.com>
2016-10-01 11:09:10 -0700

    added full function and routing for adding and deleting brands

--- app/app.php
+++ app/app.php
@@ -18,6 +18,9 @@
 
     $app = new Silex\Application();
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -37,82 +40,22 @@
         return $app->redirect('/');
     });
 
-    //brands pages
-    $app->get("addbrands", function() use ($app){
-        // goes to page to add a brand
-        return $app['twig']->render ("addbrands.html.twig");
+    $app->get('/brands', function() use ($app) {
+        //lists brands page that allows users to add and delete brands
+        return $app['twig']->render("brands.html.twig", array('brands' => Brand::getAll()));
     });
 
-    $app->post("addbrands", function() use ($app){
-        //takes brand and adds it to database
-        $name = $_POST['brand_input'];
-        $new_brand = New Brand ($name);
-        $new_brand->save();
-        return $app->redirect('/');
+    $app->post('/brands', function() use ($app) {
+        //allows the user to add a brand to the brands page
+        $brand_name = $_POST['brand_input'];
+        $brand = new Brand($brand_name);
+        $brand->save();
+        return $app['twig']->render("brands.html.twig", array('brands' => Brand::getAll()));
     });
 
-    $app->get("addbrands/{store_id}", function($store_id) use ($app){
-        //takes brand and adds it to a store
-        // $name = $_POST['brand_input'];
-        // $new_brand = New Brand ($name);
-        // $new_brand->save();
-        $store = Store::find($store_id);
-        $brands = $store->getBrandlist();
-        return $app['twig']->render ("addbrands.html.twig", array('store' => $store, 'brands' => $brands));
-    });
-
-    $app->post("addbrands/{store_id}", function($store_id) use ($app){
-        //takes brand and adds it to a store
-        $brand_id = $_POST['brand_input'];
-        $store = Store::getId($store_id);
-        $brand = Brand::getId($brand_id);
-        $store->addbrand($brand);
-        // return $app['twig']->render ("store.html.twig", array('store' => $store, 'brands' => $brands));
-        return $app->redirect('/store/' . $store.getId());
-    });
-
-    //stores pages
-    $app->get("addstores", function() use ($app) {
-        // goes to page to add a store
-        return $app['twig']->render ("addstores.html.twig");
-    });
-
-    $app->post("addstores", function() use ($app) {
-        // takes store and adds it to database
-        $name = $_POST['store_input'];
-        $new_store = New Store ($name);
-        $new_store->save();
-        return $app->redirect('/');
-    });
-
-    //store pages
-    $app->get("/stores/{store_id}", function($store_id) use ($app) {
-        //lists every brand in this store
-        $store = Store::find($store_id);
-        $brands = $store->getBrandlist();
-        return $app['twig']->render ("store.html.twig", array('store' => $store, 'brands' => $brands));
-    });
-
-    $app->post("/stores/{id}", function($id) use ($app) {
-        return $app['twig']->render ("store.html.twig");
-    });
-
-
-    // $app->update("/store{id}", function($id) use ($app) {
-    //     return $app['twig']->render ("store.html.twig");
-    // });
-    //
-    // $app->delete("/store{id}", function($id) use ($app) {
-    //     return $app['twig']->render ("store.html.twig");
-    // });
-
-    //brand pages
-    $app->get("/brand{id}", function($id) use ($app) {
-        return $app['twig']->render ("brand.html.twig");
-    });
-
-    $app->post("/brand{id}", function($id) use ($app) {
-        return $app['twig']->render ("brand.html.twig");
+    $app->post('/clearbrands', function() use ($app) {
+        Brand::deleteAll();
+        return $app->redirect('/brands');
     });
 
     return $app;
